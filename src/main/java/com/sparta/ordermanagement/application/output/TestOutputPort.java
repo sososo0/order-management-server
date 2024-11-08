442345311ab6031c6a169639d1bc3a51a5433d07
hyunsb <junghss0113@gmail.com>
2024-11-08 23:23:59 +0900

    Modify: 샘플 코드 제거

--- src/main/java/com/sparta/ordermanagement/application/output/TestOutputPort.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.ordermanagement.application.output;
-
-import com.sparta.ordermanagement.application.domain.Test;
-
-public interface TestOutputPort {
-
-    long createTest(Test test);
-}
