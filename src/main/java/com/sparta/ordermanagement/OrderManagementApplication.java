b6c7f678457955684eec1fe4a3900bce7bf009c7
hyunsb <junghss0113@gmail.com>
2024-11-08 22:54:16 +0900

    Chore: EnableJpaAuditing 활성화 엔티티의 시간정보를 자동으로 관리하기 위함

--- src/main/java/com/sparta/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/sparta/ordermanagement/OrderManagementApplication.java
@@ -2,8 +2,9 @@ package com.sparta.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OrderManagementApplication {
 
