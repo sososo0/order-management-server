71b13acff2e8f821e2265bb597a549e487658c54
hyunsb <junghss0113@gmail.com>
2024-11-12 00:42:09 +0900

    Style: 접근 제어자 명시 및 개행 추가

--- src/main/java/com/sparta/ordermanagement/framework/persistence/vo/Sort.java
+++ src/main/java/com/sparta/ordermanagement/framework/persistence/vo/Sort.java
@@ -2,6 +2,7 @@ package com.sparta.ordermanagement.framework.persistence.vo;
 
 public interface Sort {
 
-    String DEFAULT_SORT = "created_at";
+    public static final String DEFAULT_SORT = "created_at";
+
     boolean isDefaultSort();
 }
