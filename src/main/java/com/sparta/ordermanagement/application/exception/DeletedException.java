59dc1718dd74470bb85bf5a70e6f076ee0f7877d
sohyun <sh0000@pusan.ac.kr>
2024-11-12 14:39:35 +0900

    Feat: 상품 삭제 예외처리 추가 - 삭제된 상품을 조회하려고 할 때 발생

---
+++ src/main/java/com/sparta/ordermanagement/application/exception/DeletedException.java
@@ -0,0 +1,8 @@
+package com.sparta.ordermanagement.application.exception;
+
+public class DeletedException extends RuntimeException{
+
+    public DeletedException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/sparta/ordermanagement/application/exception/message/ProductErrorMessage.java
+++ src/main/java/com/sparta/ordermanagement/application/exception/message/ProductErrorMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ProductErrorMessage {
 
     PRODUCT_UUID_INVALID("유효하지 않은 상품 식별자 입니다: %s"),
-    PRODUCT_NOT_BELONG_TO_SHOP("상품 %s 는 가게 %s 에 등록되지 않은 상품입니다.");
+    PRODUCT_NOT_BELONG_TO_SHOP("상품 %s 는 가게 %s 에 등록되지 않은 상품입니다."),
+    PRODUCT_DELETED("삭제된 상품 입니다.: %s");
 
     private final String message;
 }
