3c6915986ba22f00686fc67a1c0df21ddf2ef120
eleunadeu <eleunadeu@gmail.com>
2024-11-12 11:03:50 +0900

    Modify: Order 엔티티에 배송지, 요청사항 추가 #8
    프로젝트 요구사항에 따라 OrderEntity에 주문 배송지와 요청사항 필드 추가

--- src/main/java/com/sparta/ordermanagement/framework/persistence/entity/order/OrderEntity.java
+++ src/main/java/com/sparta/ordermanagement/framework/persistence/entity/order/OrderEntity.java
@@ -1,12 +1,18 @@
 package com.sparta.ordermanagement.framework.persistence.entity.order;
 
+import com.sparta.ordermanagement.application.domain.order.Order;
+import com.sparta.ordermanagement.application.domain.order.OrderForCreate;
+import com.sparta.ordermanagement.application.domain.order.OrderForUpdate;
 import com.sparta.ordermanagement.framework.persistence.entity.BaseEntity;
+import com.sparta.ordermanagement.framework.persistence.entity.orderproduct.OrderProductEntity;
 import com.sparta.ordermanagement.framework.persistence.entity.user.UserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -28,22 +34,53 @@ public class OrderEntity extends BaseEntity {
     private OrderState orderState;
 
     @Column(nullable = false, columnDefinition = "varchar(255)")
-    private String type;
+    private OrderType orderType;
+
+    @Column(columnDefinition = "varchar(255)")
+    private String deliveryAddress;
+
+    @Column(columnDefinition = "varchar(255)")
+    private String requestOrder;
 
     @JoinColumn(nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private UserEntity userEntity;
 
-    private OrderEntity(
-            OrderState orderState, String type, UserEntity userEntity) {
+    @OneToMany(mappedBy = "orderEntity", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderProductEntity> orderProducts = new ArrayList<>();
 
+    private OrderEntity(String orderUuid, OrderState orderState,
+                        OrderType orderType, UserEntity userEntity,
+                        String deliveryAddress, String requestOrder) {
+
+        this.orderUuid = orderUuid;
         this.orderState = orderState;
-        this.type = type;
+        this.orderType = orderType;
         this.userEntity = userEntity;
+        this.deliveryAddress = deliveryAddress;
+        this.requestOrder = requestOrder;
     }
+
 
     @PrePersist
     private void prePersistence() {
         orderUuid = UUID.randomUUID().toString();
+    }
+
+    public Order toDomain() {
+
+        return new Order(id, orderUuid, orderState, orderType, userEntity.getUserId());
+    }
+
+    public static OrderEntity from(OrderForCreate orderForCreate, UserEntity userEntity) {
+
+        return new OrderEntity(
+                orderForCreate.orderId(),
+                orderForCreate.orderState(),
+                orderForCreate.orderType(),
+                userEntity,
+                orderForCreate.deliveryAddress(),
+                orderForCreate.requestOrder()
+        );
     }
 }
