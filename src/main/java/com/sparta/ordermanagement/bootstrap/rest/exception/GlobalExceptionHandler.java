5dd268608d206680f7386d6adfc486b549facc67
sohyun <sh0000@pusan.ac.kr>
2024-11-13 14:42:04 +0900

    Refactor: 요청 데이터 유효성 검사 예외 메시지 개선 - 요청 데이터의 제약 조건 위반 시, 필드별로 예외 메시지가 깔끔하게 반환되도록 예외처리 추가

--- src/main/java/com/sparta/ordermanagement/bootstrap/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/ordermanagement/bootstrap/rest/exception/GlobalExceptionHandler.java
@@ -5,10 +5,13 @@ import com.sparta.ordermanagement.application.exception.ForbiddenException;
 import com.sparta.ordermanagement.application.exception.InvalidValueException;
 import com.sparta.ordermanagement.bootstrap.rest.exception.response.ApiResponse;
 import com.sparta.ordermanagement.bootstrap.rest.exception.response.ApiResponse.Error;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -34,6 +37,20 @@ public class GlobalExceptionHandler {
         log.error("forbiddenExceptionHandle", e);
 
         return ApiResponse.error(HttpStatus.FORBIDDEN, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Error> exceptionHandle(MethodArgumentNotValidException e) {
+        log.error("methodArgumentNotValidExceptionHandle", e);
+
+        String errorMessage = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(fieldError -> fieldError.getField() + ":" + fieldError.getDefaultMessage())
+            .collect(Collectors.joining(", "));
+
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
