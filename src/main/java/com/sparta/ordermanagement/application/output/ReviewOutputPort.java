f8284eb62f9fa96e6eb998a3db05a43a059a13e1
sohyun <sh0000@pusan.ac.kr>
2024-11-14 11:17:59 +0900

    Feat: review 수정 application 로직 작성 - service에 update 로직 추가 - output port에 필요한 메서드 추가

--- src/main/java/com/sparta/ordermanagement/application/output/ReviewOutputPort.java
+++ src/main/java/com/sparta/ordermanagement/application/output/ReviewOutputPort.java
@@ -2,8 +2,14 @@ package com.sparta.ordermanagement.application.output;
 
 import com.sparta.ordermanagement.application.domain.review.Review;
 import com.sparta.ordermanagement.application.domain.review.ReviewForCreate;
+import com.sparta.ordermanagement.application.domain.review.ReviewForUpdate;
+import java.util.Optional;
 
 public interface ReviewOutputPort {
 
     Review saveReview(ReviewForCreate reviewForCreate, String shopUuid);
+
+    Review updateReview(ReviewForUpdate reviewForUpdate);
+
+    Optional<Review> findByReviewUuid(String reviewUuid);
 }
--- src/main/java/com/sparta/ordermanagement/application/service/ReviewService.java
+++ src/main/java/com/sparta/ordermanagement/application/service/ReviewService.java
@@ -3,7 +3,9 @@ package com.sparta.ordermanagement.application.service;
 import com.sparta.ordermanagement.application.domain.order.Order;
 import com.sparta.ordermanagement.application.domain.review.Review;
 import com.sparta.ordermanagement.application.domain.review.ReviewForCreate;
+import com.sparta.ordermanagement.application.domain.review.ReviewForUpdate;
 import com.sparta.ordermanagement.application.domain.shop.Shop;
+import com.sparta.ordermanagement.application.exception.review.ReviewUuidInvalidException;
 import com.sparta.ordermanagement.application.output.ReviewOutputPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,5 +26,22 @@ public class ReviewService {
         // TODO: User 확인
 
         return reviewOutputPort.saveReview(reviewForCreate, shop.getUuid());
+    }
+
+    public Review updateReview(ReviewForUpdate reviewForUpdate) {
+
+        Order order = orderService.validateOrderUuidAndGetOrder(reviewForUpdate.orderUuid());
+        shopService.validateShopUuid(order.getShopId());
+
+        validateReviewUuid(reviewForUpdate.reviewUuid());
+
+        // TODO: User 확인 && User가 Order에 속해있는지 확인
+
+        return reviewOutputPort.updateReview(reviewForUpdate);
+    }
+
+    private void validateReviewUuid(String reviewUuid) {
+        reviewOutputPort.findByReviewUuid(reviewUuid)
+            .orElseThrow(() -> new ReviewUuidInvalidException(reviewUuid));
     }
 }
