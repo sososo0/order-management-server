aef5fb6eb2e08fa20f429176cd454cae0ae97aeb
sohyun <sh0000@pusan.ac.kr>
2024-11-15 17:51:51 +0900

    Feat: 리뷰 목록 조회 시 응답 필드 추가 - 생성일, 생성자, 수정일, 수정자

--- src/main/java/com/sparta/ordermanagement/bootstrap/rest/dto/review/ReviewListResponse.java
+++ src/main/java/com/sparta/ordermanagement/bootstrap/rest/dto/review/ReviewListResponse.java
@@ -1,6 +1,7 @@
 package com.sparta.ordermanagement.bootstrap.rest.dto.review;
 
 import com.sparta.ordermanagement.application.domain.review.Review;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,19 +9,25 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ReviewListResponse {
 
-    // TODO: User 추가하기
-
     private final String reviewUuid;
     private final String userStringId;
     private final Integer rating;
     private final String content;
+    private final LocalDateTime createdAt;
+    private final String createdBy;
+    private final LocalDateTime updatedAt;
+    private final String updatedBy;
 
     public static ReviewListResponse from(Review review) {
         return new ReviewListResponse(
             review.getReviewUuid(),
-            "0000", // 추후 삭제하고 user로 대체
+            review.getUser().getUserStringId(),
             review.getRating(),
-            review.getContent()
+            review.getContent(),
+            review.getCreatedAt(),
+            review.getCreatedBy(),
+            review.getUpdatedAt(),
+            review.getUpdatedBy()
         );
     }
 }
