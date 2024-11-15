f1dae744056588afccf658f81aaa5395637b71c3
sohyun <sh0000@pusan.ac.kr>
2024-11-15 17:11:52 +0900

    Chore: 불필요한 import 제거

--- src/main/java/com/sparta/ordermanagement/bootstrap/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/ordermanagement/bootstrap/filter/JwtAuthorizationFilter.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
