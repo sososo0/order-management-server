a9123505076bd6fed11eb3ace067863374bc4b2e
hyunsb <junghss0113@gmail.com>
2024-11-15 14:20:08 +0900

    Feat: 가게의 총 별점을 계산하는 스케줄링 기능 구현 매일 오전 3시에 스케줄링 동작, 작일 오전 3시를 기준으로 생성된 리뷰를 통계

---
+++ src/main/java/com/sparta/ordermanagement/framework/persistence/scheduler/ShopScheduler.java
@@ -0,0 +1,29 @@
+package com.sparta.ordermanagement.framework.persistence.scheduler;
+
+import com.sparta.ordermanagement.framework.persistence.repository.ShopQueryRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ShopScheduler {
+
+    private static final int BASE_DAY_AGO = 1;
+    private static final int BASE_HOUR = 3;
+
+    private final ShopQueryRepository shopRepository;
+
+    @Scheduled(cron = "0 0 3 * * ?") // 매일 오전 3시
+    public void schedulingRatingStatistics() {
+        log.info("별점 통합 스케줄 작업 시작: {}", LocalDateTime.now());
+
+        LocalDateTime baseTime = LocalDateTime.now().minusDays(BASE_DAY_AGO).withHour(BASE_HOUR);
+        shopRepository.updateShopRating(baseTime);
+
+        log.info("별점 통합 스케줄 작업 완료: {}", LocalDateTime.now());
+    }
+}
