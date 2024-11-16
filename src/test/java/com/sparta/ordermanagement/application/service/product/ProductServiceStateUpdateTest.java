f3a2b0d3ad51cbb9409ccba2f333605c1be8a3e2
sohyun <sh0000@pusan.ac.kr>
2024-11-16 22:05:07 +0900

    Chore: ExtendWith 어노테이션 붙이기

--- src/test/java/com/sparta/ordermanagement/application/service/product/ProductServiceStateUpdateTest.java
+++ src/test/java/com/sparta/ordermanagement/application/service/product/ProductServiceStateUpdateTest.java
@@ -8,9 +8,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceStateUpdateTest extends BaseProductServiceTest {
 
     private String productUuid;
